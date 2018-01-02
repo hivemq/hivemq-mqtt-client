b7ed5737d61ae5be6f103fd5cff60fdffe267a85
Silvio Giebl <silvio.giebl@dc-square.de>
2018-01-02 19:20:06 +0100

    fixed PublishDecoder

--- src/main/java/org/mqttbee/mqtt5/codec/decoder/Mqtt5PublishDecoder.java
+++ src/main/java/org/mqttbee/mqtt5/codec/decoder/Mqtt5PublishDecoder.java
@@ -20,6 +20,7 @@ import javax.inject.Singleton;
 
 import static org.mqttbee.mqtt5.message.publish.Mqtt5PublishImpl.DEFAULT_MESSAGE_EXPIRY_INTERVAL_INFINITY;
 import static org.mqttbee.mqtt5.message.publish.Mqtt5PublishImpl.DEFAULT_NO_TOPIC_ALIAS;
+import static org.mqttbee.mqtt5.message.publish.Mqtt5PublishInternal.NO_PACKET_IDENTIFIER_QOS_0;
 
 /**
  * @author Silvio Giebl
@@ -47,12 +48,15 @@ public class Mqtt5PublishDecoder implements Mqtt5MessageDecoder {
             return null;
         }
 
-        if (in.readableBytes() < 2) {
-            // TODO: send Disconnect with reason code 0x81 Malformed Packet and close channel
-            in.clear();
-            return null;
+        int packetIdentifier = NO_PACKET_IDENTIFIER_QOS_0;
+        if (qos == Mqtt5QoS.AT_MOST_ONCE) {
+            if (in.readableBytes() < 2) {
+                // TODO: send Disconnect with reason code 0x81 Malformed Packet and close channel
+                in.clear();
+                return null;
+            }
+            packetIdentifier = in.readUnsignedShort();
         }
-        final int packetIdentifier = in.readUnsignedShort();
 
         final int propertiesLength = Mqtt5DataTypes.decodeVariableByteInteger(in);
         if (propertiesLength < 0) {
