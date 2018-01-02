eb21215247b8d202202c300ba068aa41876fc3cb
Silvio Giebl <silvio.giebl@dc-square.de>
2018-01-02 17:54:00 +0100

    fixed and improved UTF8String

--- src/main/java/org/mqttbee/mqtt5/message/Mqtt5ClientIdentifier.java
+++ src/main/java/org/mqttbee/mqtt5/message/Mqtt5ClientIdentifier.java
@@ -14,15 +14,14 @@ public class Mqtt5ClientIdentifier extends Mqtt5UTF8String {
 
     @NotNull
     public static final Mqtt5ClientIdentifier REQUEST_CLIENT_IDENTIFIER_FROM_SERVER =
-            new Mqtt5ClientIdentifier(encodeUnsafe(""));
+            new Mqtt5ClientIdentifier(encode(""));
     private static final Pattern MUST_BE_ALLOWED_BY_SERVER_PATTERN = Pattern.compile("([0-9]|[a-z]|[A-Z])*");
     private static final int MUST_BE_ALLOWED_BY_SERVER_MIN_BYTES = 1;
     private static final int MUST_BE_ALLOWED_BY_SERVER_MAX_BYTES = 23;
 
     @Nullable
     public static Mqtt5ClientIdentifier from(@NotNull final byte[] binary) {
-        final String string = decode(binary); // TODO: containsMustNotCharacters(byte[]) without decoding
-        return (string == null) ? null : new Mqtt5ClientIdentifier(binary, string);
+        return containsMustNotCharacters(binary) ? null : new Mqtt5ClientIdentifier(binary);
     }
 
     @Nullable
@@ -42,10 +41,6 @@ public class Mqtt5ClientIdentifier extends Mqtt5UTF8String {
 
     private Mqtt5ClientIdentifier(@NotNull final byte[] binary) {
         super(binary);
-    }
-
-    private Mqtt5ClientIdentifier(@NotNull final byte[] binary, @NotNull final String string) {
-        super(binary, string);
     }
 
     public boolean mustBeAllowedByServer() {
