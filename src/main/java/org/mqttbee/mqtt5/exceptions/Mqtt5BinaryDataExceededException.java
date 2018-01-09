7ae16fec966247a2576f56564735422213a67fe3
Silvio Giebl <silvio.giebl@dc-square.de>
2018-01-09 11:56:13 +0100

    added javadoc to exceptions

--- src/main/java/org/mqttbee/mqtt5/exceptions/Mqtt5BinaryDataExceededException.java
+++ src/main/java/org/mqttbee/mqtt5/exceptions/Mqtt5BinaryDataExceededException.java
@@ -3,10 +3,19 @@ package org.mqttbee.mqtt5.exceptions;
 import org.mqttbee.annotations.NotNull;
 
 /**
+ * Exception to indicate that a byte array can not be encoded as binary data according to the MQTT 5 specification.
+ * <p>
+ * This exception does not have a stack trace.
+ *
  * @author Silvio Giebl
  */
 public class Mqtt5BinaryDataExceededException extends RuntimeException {
 
+    /**
+     * Creates a new Mqtt5BinaryDataExceededException with the name of the binary data.
+     *
+     * @param name the name of the binary data.
+     */
     public Mqtt5BinaryDataExceededException(@NotNull final String name) {
         super("binary data size exceeded for " + name);
     }
