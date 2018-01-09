c4010558368c604894cdbbc00a1d327252db246a
Silvio Giebl <silvio.giebl@dc-square.de>
2018-01-09 09:16:14 +0100

    extended unit test skeleton for PubAck/PubComp/PubRec/PubRelDecoder

--- src/test/java/org/mqttbee/mqtt5/codec/decoder/Mqtt5PubAckDecoderTest.java
+++ src/test/java/org/mqttbee/mqtt5/codec/decoder/Mqtt5PubAckDecoderTest.java
@@ -1,15 +1,20 @@
 package org.mqttbee.mqtt5.codec.decoder;
 
+import com.google.common.collect.ImmutableList;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.embedded.EmbeddedChannel;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mqttbee.annotations.Nullable;
-import org.mqttbee.api.mqtt5.message.Mqtt5PubAck;
 import org.mqttbee.mqtt5.message.Mqtt5MessageType;
+import org.mqttbee.mqtt5.message.Mqtt5UTF8String;
+import org.mqttbee.mqtt5.message.Mqtt5UserProperty;
+import org.mqttbee.mqtt5.message.puback.Mqtt5PubAckImpl;
+import org.mqttbee.mqtt5.message.puback.Mqtt5PubAckInternal;
+import org.mqttbee.mqtt5.message.puback.Mqtt5PubAckReasonCode;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * @author Silvio Giebl
@@ -35,18 +40,46 @@ public class Mqtt5PubAckDecoderTest {
                 //   type, flags
                 0b0100_0000,
                 //   remaining length
-                0,
+                43,
                 // variable header
+                //   packet identifier
+                0, 5,
+                //   reason code (success)
+                0x00,
                 //   properties
-                0
+                39,
+                //     reason string
+                0x1F, 0, 7, 's', 'u', 'c', 'c', 'e', 's', 's',
+                //     user properties
+                0x26, 0, 4, 't', 'e', 's', 't', 0, 5, 'v', 'a', 'l', 'u', 'e',
+                0x26, 0, 4, 't', 'e', 's', 't', 0, 6, 'v', 'a', 'l', 'u', 'e', '2',
         };
 
         final ByteBuf byteBuf = channel.alloc().buffer();
         byteBuf.writeBytes(encoded);
         channel.writeInbound(byteBuf);
-        final Mqtt5PubAck pubAck = channel.readInbound();
+        final Mqtt5PubAckInternal pubAckInternal = channel.readInbound();
 
-        assertNotNull(pubAck);
+        assertNotNull(pubAckInternal);
+
+        assertEquals(5, pubAckInternal.getPacketIdentifier());
+
+        final Mqtt5PubAckImpl pubAck = pubAckInternal.getPubAck();
+
+        assertEquals(Mqtt5PubAckReasonCode.SUCCESS, pubAck.getReasonCode());
+        assertTrue(pubAck.getReasonString().isPresent());
+        assertEquals("success", pubAck.getReasonString().get().toString());
+
+        final ImmutableList<Mqtt5UserProperty> userProperties = pubAck.getUserProperties();
+        assertEquals(2, userProperties.size());
+        final Mqtt5UTF8String test = Mqtt5UTF8String.from("test");
+        final Mqtt5UTF8String value = Mqtt5UTF8String.from("value");
+        final Mqtt5UTF8String value2 = Mqtt5UTF8String.from("value2");
+        assertNotNull(test);
+        assertNotNull(value);
+        assertNotNull(value2);
+        assertTrue(userProperties.contains(new Mqtt5UserProperty(test, value)));
+        assertTrue(userProperties.contains(new Mqtt5UserProperty(test, value2)));
     }
 
     private static class Mqtt5PubAckTestMessageDecoders implements Mqtt5MessageDecoders {
--- src/test/java/org/mqttbee/mqtt5/codec/decoder/Mqtt5PubRelDecoderTest.java
+++ src/test/java/org/mqttbee/mqtt5/codec/decoder/Mqtt5PubRelDecoderTest.java
@@ -1,5 +1,6 @@
 package org.mqttbee.mqtt5.codec.decoder;
 
+import com.google.common.collect.ImmutableList;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.embedded.EmbeddedChannel;
 import org.junit.After;
@@ -8,8 +9,12 @@ import org.junit.Test;
 import org.mqttbee.annotations.Nullable;
 import org.mqttbee.api.mqtt5.message.Mqtt5PubRel;
 import org.mqttbee.mqtt5.message.Mqtt5MessageType;
+import org.mqttbee.mqtt5.message.Mqtt5UTF8String;
+import org.mqttbee.mqtt5.message.Mqtt5UserProperty;
+import org.mqttbee.mqtt5.message.pubrel.Mqtt5PubRelInternal;
+import org.mqttbee.mqtt5.message.pubrel.Mqtt5PubRelReasonCode;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * @author Silvio Giebl
@@ -33,27 +38,55 @@ public class Mqtt5PubRelDecoderTest {
         final byte[] encoded = {
                 // fixed header
                 //   type, flags
-                0b0110_0000,
+                0b0110_0010,
                 //   remaining length
-                0,
+                43,
                 // variable header
+                //   packet identifier
+                0, 5,
+                //   reason code (success)
+                0x00,
                 //   properties
-                0
+                39,
+                //     reason string
+                0x1F, 0, 7, 's', 'u', 'c', 'c', 'e', 's', 's',
+                //     user properties
+                0x26, 0, 4, 't', 'e', 's', 't', 0, 5, 'v', 'a', 'l', 'u', 'e',
+                0x26, 0, 4, 't', 'e', 's', 't', 0, 6, 'v', 'a', 'l', 'u', 'e', '2',
         };
 
         final ByteBuf byteBuf = channel.alloc().buffer();
         byteBuf.writeBytes(encoded);
         channel.writeInbound(byteBuf);
-        final Mqtt5PubRel pubRel = channel.readInbound();
+        final Mqtt5PubRelInternal pubRelInternal = channel.readInbound();
 
-        assertNotNull(pubRel);
+        assertNotNull(pubRelInternal);
+
+        assertEquals(5, pubRelInternal.getPacketIdentifier());
+
+        final Mqtt5PubRel pubRel = pubRelInternal.getPubRel();
+
+        assertEquals(Mqtt5PubRelReasonCode.SUCCESS, pubRel.getReasonCode());
+        assertTrue(pubRel.getReasonString().isPresent());
+        assertEquals("success", pubRel.getReasonString().get().toString());
+
+        final ImmutableList<Mqtt5UserProperty> userProperties = pubRel.getUserProperties();
+        assertEquals(2, userProperties.size());
+        final Mqtt5UTF8String test = Mqtt5UTF8String.from("test");
+        final Mqtt5UTF8String value = Mqtt5UTF8String.from("value");
+        final Mqtt5UTF8String value2 = Mqtt5UTF8String.from("value2");
+        assertNotNull(test);
+        assertNotNull(value);
+        assertNotNull(value2);
+        assertTrue(userProperties.contains(new Mqtt5UserProperty(test, value)));
+        assertTrue(userProperties.contains(new Mqtt5UserProperty(test, value2)));
     }
 
     private static class Mqtt5PubRelTestMessageDecoders implements Mqtt5MessageDecoders {
         @Nullable
         @Override
         public Mqtt5MessageDecoder get(final int code) {
-            if (code == Mqtt5MessageType.CONNACK.getCode()) {
+            if (code == Mqtt5MessageType.PUBREL.getCode()) {
                 return new Mqtt5PubRelDecoder();
             }
             return null;
