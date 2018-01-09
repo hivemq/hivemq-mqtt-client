68132484787c8634759cbf4bec59c11f3ac246e8
Silvio Giebl <silvio.giebl@dc-square.de>
2018-01-09 13:24:30 +0100

    added javadoc to decoder/encoder interfaces

--- src/main/java/org/mqttbee/mqtt5/codec/decoder/Mqtt5MessageDecoder.java
+++ src/main/java/org/mqttbee/mqtt5/codec/decoder/Mqtt5MessageDecoder.java
@@ -7,10 +7,21 @@ import org.mqttbee.annotations.Nullable;
 import org.mqttbee.mqtt5.message.Mqtt5Message;
 
 /**
+ * Decoder for a MQTT message according to the MQTT 5 specification.
+ *
  * @author Silvio Giebl
  */
 public interface Mqtt5MessageDecoder {
 
+    /**
+     * Decodes a MQTT message from the given byte buffer which was read from the given channel.
+     *
+     * @param flags   the flags of the fixed header.
+     * @param channel the channel the byte buffer was read from.
+     * @param in      the byte buffer which contains the encoded message without the fixed header.
+     * @return the decoded MQTT message of null if there are not enough byte in the byte buffer or if the byte buffer
+     * did not contain a valid encoded MQTT message.
+     */
     @Nullable
     Mqtt5Message decode(int flags, @NotNull Channel channel, @NotNull ByteBuf in);
 
--- src/main/java/org/mqttbee/mqtt5/codec/encoder/Mqtt5MessageEncoder.java
+++ src/main/java/org/mqttbee/mqtt5/codec/encoder/Mqtt5MessageEncoder.java
@@ -7,14 +7,29 @@ import org.mqttbee.mqtt5.codec.Mqtt5DataTypes;
 import org.mqttbee.mqtt5.message.Mqtt5Message;
 
 /**
+ * Encoder for a MQTT message according to the MQTT 5 specification.
+ *
  * @author Silvio Giebl
  */
 public interface Mqtt5MessageEncoder<T extends Mqtt5Message> {
 
+    /**
+     * Calculates the encoded length of a MQTT message with the given remaining length.
+     *
+     * @param remainingLength the remaining length of the MQTT message.
+     * @return the encoded length of the MQTT message.
+     */
     static int encodedLength(final int remainingLength) {
         return 1 + Mqtt5DataTypes.encodedVariableByteIntegerLength(remainingLength) + remainingLength;
     }
 
+    /**
+     * Encodes the given MQTT message to the given byte buffer which will be written to the given channel.
+     *
+     * @param message the MQTT message to encode.
+     * @param channel the channel where the byte buffer will be written to.
+     * @param out     the byte buffer to encode to.
+     */
     void encode(@NotNull T message, @NotNull Channel channel, @NotNull ByteBuf out);
 
 }
