0cb8bce40e497e0f637245dbd801af63849bd8fa
Silvio Giebl <silvio.giebl@dc-square.de>
2018-02-05 13:07:10 +0100

    added allocateBuffer to MqttMessage, reused ByteBuf of PingReq

--- src/main/java/org/mqttbee/mqtt5/codec/encoder/Mqtt5Encoder.java
+++ src/main/java/org/mqttbee/mqtt5/codec/encoder/Mqtt5Encoder.java
@@ -5,7 +5,6 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import org.mqttbee.mqtt5.codec.Mqtt5DataTypes;
-import org.mqttbee.mqtt5.exceptions.Mqtt5MaximumPacketSizeExceededException;
 import org.mqttbee.mqtt5.message.Mqtt5Message;
 
 import javax.inject.Inject;
@@ -28,13 +27,7 @@ public class Mqtt5Encoder extends MessageToByteEncoder<Mqtt5Message> {
 //        final Integer test = ctx.channel().attr(ChannelAttributes.OUTGOING_MAXIMUM_PACKET_SIZE).get();
 //        final int maximumPacketSize = (test == null) ? Mqtt5DataTypes.MAXIMUM_PACKET_SIZE_LIMIT : test;
         final int maximumPacketSize = Mqtt5DataTypes.MAXIMUM_PACKET_SIZE_LIMIT;
-        final int encodedLength = message.encodedLength(maximumPacketSize); // TODO
-
-        if (encodedLength < 0) {
-            throw new Mqtt5MaximumPacketSizeExceededException(message, maximumPacketSize);
-        }
-
-        return ctx.alloc().ioBuffer(encodedLength, encodedLength);
+        return message.allocateBuffer(maximumPacketSize, ctx.channel());
     }
 
     @Override
