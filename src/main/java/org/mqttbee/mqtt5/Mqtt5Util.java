4936cf2c88ea37c00fe9f57759ba062e03dcfb8b
Silvio Giebl <silvio.giebl@dc-square.de>
2018-02-07 08:22:23 +0100

    rebase fix

--- src/main/java/org/mqttbee/mqtt5/Mqtt5Util.java
+++ src/main/java/org/mqttbee/mqtt5/Mqtt5Util.java
@@ -5,8 +5,8 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import org.mqttbee.annotations.NotNull;
 import org.mqttbee.annotations.Nullable;
-import org.mqttbee.mqtt5.message.Mqtt5UTF8String;
-import org.mqttbee.mqtt5.message.Mqtt5UserProperty;
+import org.mqttbee.mqtt5.message.Mqtt5UTF8StringImpl;
+import org.mqttbee.mqtt5.message.Mqtt5UserPropertiesImpl;
 import org.mqttbee.mqtt5.message.disconnect.Mqtt5DisconnectImpl;
 import org.mqttbee.mqtt5.message.disconnect.Mqtt5DisconnectReasonCode;
 
@@ -21,17 +21,17 @@ public class Mqtt5Util {
 
         channel.config().setAutoRead(false);
 
-        Mqtt5UTF8String mqttReasonString = null;
+        Mqtt5UTF8StringImpl mqttReasonString = null;
         if (reasonString != null) {
             final Boolean sendReasonString = channel.attr(ChannelAttributes.SEND_REASON_STRING).get();
             if ((sendReasonString != null) && sendReasonString) {
-                mqttReasonString = Mqtt5UTF8String.from(reasonString);
+                mqttReasonString = Mqtt5UTF8StringImpl.from(reasonString);
             }
         }
 
-        final Mqtt5DisconnectImpl disconnect = new Mqtt5DisconnectImpl(
-                reasonCode, Mqtt5DisconnectImpl.SESSION_EXPIRY_INTERVAL_FROM_CONNECT, null, mqttReasonString,
-                Mqtt5UserProperty.DEFAULT_NO_USER_PROPERTIES);
+        final Mqtt5DisconnectImpl disconnect =
+                new Mqtt5DisconnectImpl(reasonCode, Mqtt5DisconnectImpl.SESSION_EXPIRY_INTERVAL_FROM_CONNECT, null,
+                        mqttReasonString, Mqtt5UserPropertiesImpl.NO_USER_PROPERTIES);
         final ChannelFuture disconnectFuture = channel.writeAndFlush(disconnect);
 
         disconnectFuture.addListener(ChannelFutureListener.CLOSE);
