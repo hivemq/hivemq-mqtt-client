fdc77a3d003d3b71c5b71dc1e4a50dcf769695d6
Silvio Giebl <silvio.giebl@dc-square.de>
2018-02-14 12:08:18 +0100

    Removed Impl from ExtendedAuthBuilder

--- src/main/java/org/mqttbee/api/mqtt5/message/auth/Mqtt5ExtendedAuthBuilder.java
+++ src/main/java/org/mqttbee/api/mqtt5/message/auth/Mqtt5ExtendedAuthBuilder.java
@@ -3,7 +3,6 @@ package org.mqttbee.api.mqtt5.message.auth;
 import org.mqttbee.annotations.DoNotImplement;
 import org.mqttbee.annotations.NotNull;
 import org.mqttbee.annotations.Nullable;
-import org.mqttbee.mqtt5.message.auth.Mqtt5ExtendedAuthBuilderImpl;
 
 import java.nio.ByteBuffer;
 
@@ -14,9 +13,9 @@ import java.nio.ByteBuffer;
 public interface Mqtt5ExtendedAuthBuilder {
 
     @NotNull
-    Mqtt5ExtendedAuthBuilderImpl withData(@Nullable byte[] data);
+    Mqtt5ExtendedAuthBuilder withData(@Nullable byte[] data);
 
     @NotNull
-    Mqtt5ExtendedAuthBuilderImpl withData(@Nullable ByteBuffer data);
+    Mqtt5ExtendedAuthBuilder withData(@Nullable ByteBuffer data);
 
 }
