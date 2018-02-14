d74af8c33e22b65f0e709b2379238e029f2492d1
Silvio Giebl <silvio.giebl@dc-square.de>
2018-02-14 08:45:08 +0100

    Added executor to MqttClientBuilder

--- src/main/java/org/mqttbee/api/MqttClientBuilder.java
+++ src/main/java/org/mqttbee/api/MqttClientBuilder.java
@@ -1,10 +1,13 @@
 package org.mqttbee.api;
 
+import com.google.common.base.Preconditions;
 import org.mqttbee.annotations.NotNull;
 import org.mqttbee.api.mqtt5.Mqtt5ClientBuilder;
 import org.mqttbee.api.mqtt5.message.Mqtt5ClientIdentifier;
 import org.mqttbee.mqtt5.message.Mqtt5ClientIdentifierImpl;
 import org.mqttbee.util.MustNotBeImplementedUtil;
+
+import java.util.concurrent.Executor;
 
 /**
  * @author Silvio Giebl
@@ -14,6 +17,8 @@ public class MqttClientBuilder {
     private Mqtt5ClientIdentifierImpl identifier = Mqtt5ClientIdentifierImpl.REQUEST_CLIENT_IDENTIFIER_FROM_SERVER;
     private String host = "localhost";
     private int port = 1883;
+    private Executor executor;
+    private int numberOfNettyThreads;
 
     @NotNull
     public MqttClientBuilder withIdentifier(@NotNull final Mqtt5ClientIdentifier identifier) {
@@ -34,8 +39,22 @@ public class MqttClientBuilder {
     }
 
     @NotNull
+    public MqttClientBuilder using(@NotNull final Executor executor) {
+        Preconditions.checkNotNull(executor);
+        this.executor = executor;
+        return this;
+    }
+
+    @NotNull
+    public MqttClientBuilder using(final int numberOfNettyThreads) {
+        Preconditions.checkArgument(numberOfNettyThreads > 0);
+        this.numberOfNettyThreads = numberOfNettyThreads;
+        return this;
+    }
+
+    @NotNull
     public Mqtt5ClientBuilder useMqtt5() {
-        return new Mqtt5ClientBuilder(identifier, host, port);
+        return new Mqtt5ClientBuilder(identifier, host, port, executor, numberOfNettyThreads);
     }
 
 }
