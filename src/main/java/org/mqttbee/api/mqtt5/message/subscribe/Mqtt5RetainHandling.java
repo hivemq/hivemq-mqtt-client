b40ae819140a64d51c22f14cf0945b6538da8665
Silvio Giebl <silvio.giebl@dc-square.de>
2018-02-14 10:22:53 +0100

    Added missing javadoc

--- src/main/java/org/mqttbee/api/mqtt5/message/subscribe/Mqtt5RetainHandling.java
+++ src/main/java/org/mqttbee/api/mqtt5/message/subscribe/Mqtt5RetainHandling.java
@@ -3,6 +3,8 @@ package org.mqttbee.api.mqtt5.message.subscribe;
 import org.mqttbee.annotations.Nullable;
 
 /**
+ * Retain Handling according to the MQTT 5 specification.
+ *
  * @author Silvio Giebl
  */
 public enum Mqtt5RetainHandling {
@@ -11,10 +13,19 @@ public enum Mqtt5RetainHandling {
     SEND_IF_SUBSCRIPTION_DOES_NOT_EXIST,
     DO_NOT_SEND;
 
+    /**
+     * @return the byte code of this Retain Handling.
+     */
     public int getCode() {
         return ordinal();
     }
 
+    /**
+     * Returns the Retain Handling belonging to the given byte code.
+     *
+     * @param code the byte code.
+     * @return the Retain Handling belonging to the byte code or null if the byte code is not a valid Retain Handling.
+     */
     @Nullable
     public static Mqtt5RetainHandling fromCode(final int code) {
         final Mqtt5RetainHandling[] values = values();
--- src/main/java/org/mqttbee/api/mqtt5/message/subscribe/Mqtt5SubscribeResult.java
+++ src/main/java/org/mqttbee/api/mqtt5/message/subscribe/Mqtt5SubscribeResult.java
@@ -1,6 +1,8 @@
 package org.mqttbee.api.mqtt5.message.subscribe;
 
 /**
+ * Marker interface for MQTT messages sent by the server because of a Subscribe message sent by the client.
+ *
  * @author Silvio Giebl
  */
 public interface Mqtt5SubscribeResult {
