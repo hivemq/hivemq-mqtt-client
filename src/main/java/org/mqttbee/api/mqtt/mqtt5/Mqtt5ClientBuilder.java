a74ba4cfc183a33242a1eca37edfb3485ee8d649
Christian Hoff <Christian.Hoff@bmw-carit.de>
2018-06-15 15:57:27 +0200

    switching MQTT version throws unsupported operation exception

--- src/main/java/org/mqttbee/api/mqtt/mqtt5/Mqtt5ClientBuilder.java
+++ src/main/java/org/mqttbee/api/mqtt/mqtt5/Mqtt5ClientBuilder.java
@@ -25,6 +25,7 @@ import org.mqttbee.api.mqtt.MqttClientExecutorConfig;
 import org.mqttbee.api.mqtt.MqttClientSslConfig;
 import org.mqttbee.api.mqtt.MqttWebsocketConfig;
 import org.mqttbee.api.mqtt.datatypes.MqttClientIdentifier;
+import org.mqttbee.api.mqtt.mqtt3.Mqtt3ClientBuilder;
 import org.mqttbee.api.mqtt.mqtt5.advanced.Mqtt5AdvancedClientData;
 import org.mqttbee.mqtt.MqttClientData;
 import org.mqttbee.mqtt.MqttClientExecutorConfigImpl;
@@ -127,6 +128,19 @@ public class Mqtt5ClientBuilder extends MqttClientBuilder {
     @Override
     public Mqtt5ClientBuilder executorConfig(@NotNull final MqttClientExecutorConfig executorConfig) {
         super.executorConfig(executorConfig);
+        return this;
+    }
+
+    @NotNull
+    @Override
+    public Mqtt3ClientBuilder useMqttVersion3() {
+        throw new UnsupportedOperationException(
+                "Switching MQTT Version is not allowed. Please call useMqttVersion3/5 only once.");
+    }
+
+    @NotNull
+    @Override
+    public Mqtt5ClientBuilder useMqttVersion5() {
         return this;
     }
 
