9dabd1840398d8f80a847afb49ce33f85d077c2e
Silvio Giebl <silvio.giebl@dc-square.de>
2019-03-21 11:56:50 +0100

    Made testing of nonnull fields in equals/hashCode explicit

--- src/test/java/com/hivemq/client/internal/mqtt/message/connect/MqttConnectRestrictionsTest.java
+++ src/test/java/com/hivemq/client/internal/mqtt/message/connect/MqttConnectRestrictionsTest.java
@@ -19,6 +19,7 @@ package com.hivemq.client.internal.mqtt.message.connect;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -28,6 +29,9 @@ class MqttConnectRestrictionsTest {
 
     @Test
     void equals() {
-        EqualsVerifier.forClass(MqttConnectRestrictions.class).suppress(Warning.STRICT_INHERITANCE).verify();
+        EqualsVerifier.forClass(MqttConnectRestrictions.class)
+                .withIgnoredAnnotations(NotNull.class) // EqualsVerifier thinks @NotNull Optional is @NotNull
+                .suppress(Warning.STRICT_INHERITANCE)
+                .verify();
     }
 }
