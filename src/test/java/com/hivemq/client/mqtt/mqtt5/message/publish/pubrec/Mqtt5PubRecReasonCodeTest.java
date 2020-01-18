cdef16cba40986f1076f322ff7397e477db9844f
Silvio Giebl <silvio.giebl@hivemq.com>
2020-01-18 15:16:04 +0100

    Add unit tests for enums in package com.hivemq.client.mqtt DisconnectReasonCode canBeSentByClient: add MALFORMED_PACKET, BAD_AUTHENTICATION_METHOD, PAYLOAD_FORMAT_INVALID, remove TOPIC_FILTER_INVALID DisconnectReasonCode canBeSetByUser: add PAYLOAD_FORMAT_INVALID

--- src/test/java/com/hivemq/client/mqtt/mqtt5/message/publish/pubrec/Mqtt5PubRecReasonCodeTest.java
+++ src/test/java/com/hivemq/client/mqtt/mqtt5/message/publish/pubrec/Mqtt5PubRecReasonCodeTest.java
@@ -15,7 +15,7 @@
  *
  */
 
-package com.hivemq.client.mqtt.mqtt5.message.publish.puback;
+package com.hivemq.client.mqtt.mqtt5.message.publish.pubrec;
 
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
@@ -29,94 +29,93 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * @author Christian Hoff
  * @author Silvio Giebl
  */
-class Mqtt5PubAckReasonCodeTest {
+class Mqtt5PubRecReasonCodeTest {
 
     @ParameterizedTest
     @MethodSource("provideCodes")
-    void getCode(final @NotNull Mqtt5PubAckReasonCode reasonCode, final int code) {
+    void getCode(final @NotNull Mqtt5PubRecReasonCode reasonCode, final int code) {
         assertEquals(code, reasonCode.getCode());
     }
 
     @ParameterizedTest
     @MethodSource("provideCodes")
-    void fromCode(final @NotNull Mqtt5PubAckReasonCode reasonCode, final int code) {
-        assertEquals(reasonCode, Mqtt5PubAckReasonCode.fromCode(code));
+    void fromCode(final @NotNull Mqtt5PubRecReasonCode reasonCode, final int code) {
+        assertEquals(reasonCode, Mqtt5PubRecReasonCode.fromCode(code));
     }
 
     @Test
     void fromCode_invalidCodes() {
-        assertNull(Mqtt5PubAckReasonCode.fromCode(0xFF));
-        assertNull(Mqtt5PubAckReasonCode.fromCode(-1));
+        assertNull(Mqtt5PubRecReasonCode.fromCode(0xFF));
+        assertNull(Mqtt5PubRecReasonCode.fromCode(-1));
     }
 
     private static @NotNull Stream<Arguments> provideCodes() {
         return Stream.of(
-                Arguments.of(Mqtt5PubAckReasonCode.SUCCESS, 0x00),
-                Arguments.of(Mqtt5PubAckReasonCode.NO_MATCHING_SUBSCRIBERS, 0x10),
-                Arguments.of(Mqtt5PubAckReasonCode.UNSPECIFIED_ERROR, 0x80),
-                Arguments.of(Mqtt5PubAckReasonCode.IMPLEMENTATION_SPECIFIC_ERROR, 0x83),
-                Arguments.of(Mqtt5PubAckReasonCode.NOT_AUTHORIZED, 0x87),
-                Arguments.of(Mqtt5PubAckReasonCode.TOPIC_NAME_INVALID, 0x90),
-                Arguments.of(Mqtt5PubAckReasonCode.PACKET_IDENTIFIER_IN_USE, 0x91),
-                Arguments.of(Mqtt5PubAckReasonCode.QUOTA_EXCEEDED, 0x97),
-                Arguments.of(Mqtt5PubAckReasonCode.PAYLOAD_FORMAT_INVALID, 0x99));
+                Arguments.of(Mqtt5PubRecReasonCode.SUCCESS, 0x00),
+                Arguments.of(Mqtt5PubRecReasonCode.NO_MATCHING_SUBSCRIBERS, 0x10),
+                Arguments.of(Mqtt5PubRecReasonCode.UNSPECIFIED_ERROR, 0x80),
+                Arguments.of(Mqtt5PubRecReasonCode.IMPLEMENTATION_SPECIFIC_ERROR, 0x83),
+                Arguments.of(Mqtt5PubRecReasonCode.NOT_AUTHORIZED, 0x87),
+                Arguments.of(Mqtt5PubRecReasonCode.TOPIC_NAME_INVALID, 0x90),
+                Arguments.of(Mqtt5PubRecReasonCode.PACKET_IDENTIFIER_IN_USE, 0x91),
+                Arguments.of(Mqtt5PubRecReasonCode.QUOTA_EXCEEDED, 0x97),
+                Arguments.of(Mqtt5PubRecReasonCode.PAYLOAD_FORMAT_INVALID, 0x99));
     }
 
     @ParameterizedTest
-    @EnumSource(value = Mqtt5PubAckReasonCode.class, names = {"SUCCESS", "NO_MATCHING_SUBSCRIBERS"})
-    void isError_false(final @NotNull Mqtt5PubAckReasonCode reasonCode) {
+    @EnumSource(value = Mqtt5PubRecReasonCode.class, names = {"SUCCESS", "NO_MATCHING_SUBSCRIBERS"})
+    void isError_false(final @NotNull Mqtt5PubRecReasonCode reasonCode) {
         assertFalse(reasonCode.isError());
     }
 
     @ParameterizedTest
-    @EnumSource(value = Mqtt5PubAckReasonCode.class, names = {
+    @EnumSource(value = Mqtt5PubRecReasonCode.class, names = {
             "UNSPECIFIED_ERROR", "IMPLEMENTATION_SPECIFIC_ERROR", "NOT_AUTHORIZED", "TOPIC_NAME_INVALID",
             "PACKET_IDENTIFIER_IN_USE", "QUOTA_EXCEEDED", "PAYLOAD_FORMAT_INVALID"
     })
-    void isError_true(final @NotNull Mqtt5PubAckReasonCode reasonCode) {
+    void isError_true(final @NotNull Mqtt5PubRecReasonCode reasonCode) {
         assertTrue(reasonCode.isError());
     }
 
     @ParameterizedTest
-    @EnumSource(value = Mqtt5PubAckReasonCode.class, names = {
+    @EnumSource(value = Mqtt5PubRecReasonCode.class, names = {
             "SUCCESS", "NO_MATCHING_SUBSCRIBERS", "UNSPECIFIED_ERROR", "IMPLEMENTATION_SPECIFIC_ERROR",
             "NOT_AUTHORIZED", "TOPIC_NAME_INVALID", "PACKET_IDENTIFIER_IN_USE", "QUOTA_EXCEEDED",
             "PAYLOAD_FORMAT_INVALID"
     })
-    void canBeSentByServer_true(final @NotNull Mqtt5PubAckReasonCode reasonCode) {
+    void canBeSentByServer_true(final @NotNull Mqtt5PubRecReasonCode reasonCode) {
         assertTrue(reasonCode.canBeSentByServer());
     }
 
     @ParameterizedTest
-    @EnumSource(value = Mqtt5PubAckReasonCode.class, names = {
+    @EnumSource(value = Mqtt5PubRecReasonCode.class, names = {
             "SUCCESS", "UNSPECIFIED_ERROR", "IMPLEMENTATION_SPECIFIC_ERROR", "NOT_AUTHORIZED", "TOPIC_NAME_INVALID",
             "PACKET_IDENTIFIER_IN_USE", "QUOTA_EXCEEDED", "PAYLOAD_FORMAT_INVALID"
     })
-    void canBeSentByClient_true(final @NotNull Mqtt5PubAckReasonCode reasonCode) {
+    void canBeSentByClient_true(final @NotNull Mqtt5PubRecReasonCode reasonCode) {
         assertTrue(reasonCode.canBeSentByClient());
     }
 
     @ParameterizedTest
-    @EnumSource(value = Mqtt5PubAckReasonCode.class, names = {"NO_MATCHING_SUBSCRIBERS"})
-    void canBeSentByClient_false(final @NotNull Mqtt5PubAckReasonCode reasonCode) {
+    @EnumSource(value = Mqtt5PubRecReasonCode.class, names = {"NO_MATCHING_SUBSCRIBERS"})
+    void canBeSentByClient_false(final @NotNull Mqtt5PubRecReasonCode reasonCode) {
         assertFalse(reasonCode.canBeSentByClient());
     }
 
     @ParameterizedTest
-    @EnumSource(value = Mqtt5PubAckReasonCode.class, names = {
+    @EnumSource(value = Mqtt5PubRecReasonCode.class, names = {
             "SUCCESS", "UNSPECIFIED_ERROR", "IMPLEMENTATION_SPECIFIC_ERROR", "NOT_AUTHORIZED", "TOPIC_NAME_INVALID",
             "QUOTA_EXCEEDED", "PAYLOAD_FORMAT_INVALID"
     })
-    void canBeSetByUser_true(final @NotNull Mqtt5PubAckReasonCode reasonCode) {
+    void canBeSetByUser_true(final @NotNull Mqtt5PubRecReasonCode reasonCode) {
         assertTrue(reasonCode.canBeSetByUser());
     }
 
     @ParameterizedTest
-    @EnumSource(value = Mqtt5PubAckReasonCode.class, names = {"NO_MATCHING_SUBSCRIBERS", "PACKET_IDENTIFIER_IN_USE"})
-    void canBeSetByUser_false(final @NotNull Mqtt5PubAckReasonCode reasonCode) {
+    @EnumSource(value = Mqtt5PubRecReasonCode.class, names = {"NO_MATCHING_SUBSCRIBERS", "PACKET_IDENTIFIER_IN_USE"})
+    void canBeSetByUser_false(final @NotNull Mqtt5PubRecReasonCode reasonCode) {
         assertFalse(reasonCode.canBeSetByUser());
     }
 }
