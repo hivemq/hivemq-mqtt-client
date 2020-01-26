cd3429c81a6a729e85ba460c1380823278521a30
Silvio Giebl <silvio.giebl@hivemq.com>
2020-01-26 20:05:01 +0100

    Simplified and optimized websocket pipeline

--- src/main/java/com/hivemq/client/internal/mqtt/handler/websocket/MqttWebSocketInitializer.java
+++ src/main/java/com/hivemq/client/internal/mqtt/handler/websocket/MqttWebSocketInitializer.java
@@ -25,11 +25,14 @@ import com.hivemq.client.internal.mqtt.ioc.ConnectionScope;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpClientCodec;
 import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.websocketx.WebSocketClientProtocolHandler;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory;
 import io.netty.handler.codec.http.websocketx.WebSocketVersion;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
 import java.net.URI;
@@ -42,7 +45,6 @@ import java.net.URISyntaxException;
 public class MqttWebSocketInitializer extends ChannelInboundHandlerAdapter {
 
     private static final @NotNull String NAME = "ws.init";
-    private static final @NotNull String PROTOCOL_HANDLER_NAME = "ws.protocol";
     private static final @NotNull String HTTP_CODEC_NAME = "http.codec";
     private static final @NotNull String HTTP_AGGREGATOR_NAME = "http.aggregator";
 
@@ -53,6 +55,8 @@ public class MqttWebSocketInitializer extends ChannelInboundHandlerAdapter {
 
     private final @NotNull MqttChannelInitializer mqttChannelInitializer;
     private final @NotNull MqttWebSocketCodec mqttWebSocketCodec;
+
+    private @Nullable WebSocketClientHandshaker handshaker;
 
     @Inject
     MqttWebSocketInitializer(
@@ -67,33 +71,48 @@ public class MqttWebSocketInitializer extends ChannelInboundHandlerAdapter {
     public void initChannel(final @NotNull Channel channel, final @NotNull MqttWebSocketConfigImpl webSocketConfig)
             throws URISyntaxException {
 
-        final HttpClientCodec httpCodec = new HttpClientCodec();
-        final HttpObjectAggregator httpAggregator =
-                new HttpObjectAggregator(MqttVariableByteInteger.MAXIMUM_PACKET_SIZE_LIMIT);
-
         final URI uri = new URI((clientConfig.getTransportConfig().getRawSslConfig() == null) ? WEBSOCKET_URI_SCHEME :
                 WEBSOCKET_TLS_URI_SCHEME, null, clientConfig.getServerHost(), clientConfig.getServerPort(),
                 "/" + webSocketConfig.getServerPath(), null, null);
 
-        final WebSocketClientProtocolHandler webSocketClientProtocolHandler =
-                new WebSocketClientProtocolHandler(uri, WebSocketVersion.V13, webSocketConfig.getSubprotocol(), true,
-                        null, MqttVariableByteInteger.MAXIMUM_PACKET_SIZE_LIMIT);
+        handshaker = WebSocketClientHandshakerFactory.newHandshaker(uri, WebSocketVersion.V13,
+                webSocketConfig.getSubprotocol(), true, null, MqttVariableByteInteger.MAXIMUM_PACKET_SIZE_LIMIT, true,
+                false);
 
         channel.pipeline()
-                .addLast(HTTP_CODEC_NAME, httpCodec)
-                .addLast(HTTP_AGGREGATOR_NAME, httpAggregator)
-                .addLast(PROTOCOL_HANDLER_NAME, webSocketClientProtocolHandler)
+                .addLast(HTTP_CODEC_NAME, new HttpClientCodec())
+                .addLast(HTTP_AGGREGATOR_NAME, new HttpObjectAggregator(65_535))
                 .addLast(NAME, this)
                 .addLast(MqttWebSocketCodec.NAME, mqttWebSocketCodec);
     }
 
     @Override
-    public void userEventTriggered(final @NotNull ChannelHandlerContext ctx, final @NotNull Object evt) {
-        if (evt == WebSocketClientProtocolHandler.ClientHandshakeStateEvent.HANDSHAKE_COMPLETE) {
-            mqttChannelInitializer.initMqtt(ctx.channel());
-            ctx.pipeline().remove(this);
+    public void channelActive(final ChannelHandlerContext ctx) {
+        assert handshaker != null;
+
+        ctx.fireChannelActive();
+        handshaker.handshake(ctx.channel(), ctx.voidPromise());
+    }
+
+    @Override
+    public void channelRead(final @NotNull ChannelHandlerContext ctx, final @NotNull Object msg) {
+        assert handshaker != null;
+
+        if (msg instanceof FullHttpResponse) {
+            final FullHttpResponse response = (FullHttpResponse) msg;
+            try {
+                if (handshaker.isHandshakeComplete()) {
+                    throw new IllegalStateException(
+                            "Must not receive http response if websocket handshake is already finished.");
+                }
+                handshaker.finishHandshake(ctx.channel(), response);
+                mqttChannelInitializer.initMqtt(ctx.channel());
+                ctx.pipeline().remove(this);
+            } finally {
+                response.release();
+            }
         } else {
-            ctx.fireUserEventTriggered(evt);
+            ctx.fireChannelRead(msg);
         }
     }
 
