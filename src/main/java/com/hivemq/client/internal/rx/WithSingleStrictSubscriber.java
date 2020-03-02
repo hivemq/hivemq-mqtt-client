0b7cf0dd5f68c5ebec0210696a3f576b38396343
Silvio Giebl <silvio.giebl@hivemq.com>
2020-03-02 08:21:57 +0100

    Improved FlowableWithSingleCombine and WithSingleStrictSubscriber

--- src/main/java/com/hivemq/client/internal/rx/WithSingleStrictSubscriber.java
+++ src/main/java/com/hivemq/client/internal/rx/WithSingleStrictSubscriber.java
@@ -25,6 +25,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.reactivestreams.Subscription;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -36,6 +37,8 @@ public class WithSingleStrictSubscriber<F, S> implements FlowableWithSingleSubsc
     private final @NotNull WithSingleSubscriber<F, S> subscriber;
     private final @NotNull AtomicReference<@Nullable Subscription> subscription = new AtomicReference<>();
     private final @NotNull AtomicLong requested = new AtomicLong();
+    private final @NotNull AtomicInteger wip = new AtomicInteger();
+    private @Nullable Throwable error;
 
     public WithSingleStrictSubscriber(final @NotNull WithSingleSubscriber<F, S> subscriber) {
         this.subscriber = subscriber;
@@ -43,14 +46,20 @@ public class WithSingleStrictSubscriber<F, S> implements FlowableWithSingleSubsc
 
     @Override
     public void onSubscribe(final @NotNull Subscription subscription) {
-        subscriber.onSubscribe(subscription);
-        if (this.subscription.compareAndSet(null, subscription)) {
-            final long requested = this.requested.getAndSet(0);
-            if (requested != 0) {
-                subscription.request(requested);
+        if (this.subscription.compareAndSet(null, this)) {
+            subscriber.onSubscribe(this);
+            if (this.subscription.compareAndSet(this, subscription)) {
+                final long requested = this.requested.getAndSet(0);
+                if (requested != 0) {
+                    subscription.request(requested);
+                }
+            } else {
+                subscription.cancel();
             }
         } else {
             subscription.cancel();
+            cancel();
+            onError(new IllegalStateException("§2.12 violated: onSubscribe must be called at most once"));
         }
     }
 
@@ -61,31 +70,52 @@ public class WithSingleStrictSubscriber<F, S> implements FlowableWithSingleSubsc
 
     @Override
     public void onNext(final @NotNull F f) {
-        subscriber.onNext(f);
+        if (wip.compareAndSet(0, 1)) {
+            subscriber.onNext(f);
+            if (wip.decrementAndGet() != 0) {
+                if (error == null) {
+                    subscriber.onComplete();
+                } else {
+                    subscriber.onError(error);
+                    error = null;
+                }
+            }
+        }
     }
 
     @Override
     public void onError(final @NotNull Throwable throwable) {
-        subscriber.onError(throwable);
+        error = throwable;
+        if (wip.getAndIncrement() == 0) {
+            subscriber.onError(throwable);
+            error = null;
+        }
     }
 
     @Override
     public void onComplete() {
-        subscriber.onComplete();
+        if (wip.getAndIncrement() == 0) {
+            subscriber.onComplete();
+        }
     }
 
     @Override
     public void request(final long n) {
-        Subscription subscription = this.subscription.get();
-        if (subscription != null) {
-            subscription.request(n);
+        if (n <= 0) {
+            cancel();
+            onError(new IllegalArgumentException("§3.9 violated: positive request amount required but it was " + n));
         } else {
-            BackpressureHelper.add(requested, n);
-            subscription = this.subscription.get();
-            if (subscription != null) {
-                final long requested = this.requested.getAndSet(0);
-                if (requested != 0) {
-                    subscription.request(requested);
+            Subscription subscription = this.subscription.get();
+            if ((subscription != null) && (subscription != this)) {
+                subscription.request(n);
+            } else {
+                BackpressureHelper.add(requested, n);
+                subscription = this.subscription.get();
+                if ((subscription != null) && (subscription != this)) {
+                    final long requested = this.requested.getAndSet(0);
+                    if (requested != 0) {
+                        subscription.request(requested);
+                    }
                 }
             }
         }
@@ -94,7 +124,7 @@ public class WithSingleStrictSubscriber<F, S> implements FlowableWithSingleSubsc
     @Override
     public void cancel() {
         final Subscription subscription = this.subscription.getAndSet(SubscriptionHelper.CANCELLED);
-        if ((subscription != null) && (subscription != SubscriptionHelper.CANCELLED)) {
+        if ((subscription != null) && (subscription != this) && (subscription != SubscriptionHelper.CANCELLED)) {
             subscription.cancel();
         }
     }
